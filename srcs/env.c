f9a80e86a361092a98268126f728638936d92e8c
amiguez <amiguez@student.42lyon.fr>
2022-10-11 18:56:02 +0200

    better env_is_set return of error to use properly

--- srcs/env.c
+++ srcs/env.c
@@ -6,7 +6,7 @@
 /*   By: amiguez <amiguez@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/06 02:51:28 by amiguez           #+#    #+#             */
-/*   Updated: 2022/10/11 18:23:11 by amiguez          ###   ########.fr       */
+/*   Updated: 2022/10/11 18:55:24 by amiguez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,45 @@
 
 /**
  * @brief Retrurn ENV_NOTSET, ENV_UNSET or ENV_SET 
- * 	Return -1 if the name is not valuale Not implemented yet
+ * 	Return -1 if the name is not valuale Not implemented yet or crash
  * 
  * @param env 
  * @param name 
  */
 int	env_is_set(char **env, char *name)
 {
-	char	*name_;
+	char	**temp;
 	int		i;
 
 	i = 0;
-	name_ = ft_strjoin(name, "=");
+	temp = ft_split(name, '=');
+	if (!temp)
+		return (-1);
+	if (temp[0][ft_strlen(temp[0])] == '+')
+		temp[0][ft_strlen(temp[0])] = 0;
+	if (temp[0][0] != '_' && ft_isalpha(temp[0][0]))
+		return (-2);	//need a print on that bad boy
 	while (env[i])
 	{
-		if (!ft_strncmp(env[i], name_, ft_strlen(name_)))
-			return (ENV_SET);
-		if (!ft_strncmp(env[i], name, ft_strlen(name) - 1) && \
-				env[i][ft_strlen(name)] == 0)
-			return (ENV_UNSET);
+		if (!ft_strncmp(env[i], temp[0], ft_strlen(temp[0])))
+		{
+			if (env[i][ft_strlen(temp[0])] == '=')
+			{
+				free(temp);
+				return (ENV_SET);
+			}
+		}
+		if (!ft_strncmp(env[i], temp[0], ft_strlen(temp[0])))
+		{
+			if (env[i][ft_strlen(temp[0])] == 0)
+			{
+				free(temp);
+				return (ENV_NOTSET);
+			}
+		}
 		i++;
 	}
-	free(name_);
+	free(temp);
 	return (ENV_NOTSET);
 }
 
