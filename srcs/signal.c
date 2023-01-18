ea150de43639456ad76384c43acb484707b24f1d
amiguez <amiguez@student.42lyon.fr>
2023-01-18 17:30:49 +0100

    correct handle of C-c

--- srcs/signal.c
+++ srcs/signal.c
@@ -6,7 +6,7 @@
 /*   By: amiguez <amiguez@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 20:00:51 by amiguez           #+#    #+#             */
-/*   Updated: 2023/01/17 19:34:38 by amiguez          ###   ########.fr       */
+/*   Updated: 2023/01/18 17:23:43 by amiguez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 void	sigint_handler(int sig)
 {
 	(void)sig;
+	rl_redisplay();
 	ft_putstr_fd("  \b\b", 2);
 	rl_on_new_line();
 	ft_putchar_fd('\n', 2);
