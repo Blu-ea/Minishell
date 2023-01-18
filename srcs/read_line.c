2217b01bb79b14ce68df4ea78dbc28e46be2ab34
amiguez <amiguez@student.42lyon.fr>
2023-01-18 18:24:49 +0100

    Force an input for parsing and exec

--- srcs/read_line.c
+++ srcs/read_line.c
@@ -6,7 +6,7 @@
 /*   By: amiguez <amiguez@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 18:38:16 by amiguez           #+#    #+#             */
-/*   Updated: 2023/01/17 18:24:05 by amiguez          ###   ########.fr       */
+/*   Updated: 2023/01/18 18:23:19 by amiguez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,8 @@ char	*ft_read_line(char **env)
 	if (g_error_sig)
 		env = exp_update (env, "?=1");
 	g_error_sig = 0;
+	if (!*line)
+		return (ft_read_line(env));
 	return (line);
 }
 
